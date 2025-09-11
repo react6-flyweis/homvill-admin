56ef960cec95cd80f01e95b0119b9cbd8c6d0e0d
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-11 15:51:53 +0530

    feat: initialize components library

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: "",
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
