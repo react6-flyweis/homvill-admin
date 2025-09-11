7f41e71c42e58298046aae30c4c980d2a5680ce0
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-11 20:13:00 +0530

    feat: add LoadingScreen component and wrap routes in Suspense for improved loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
 import { authRoutes, routes } from "./routes";
+import { LoadingScreen } from "./components/layouts/LoadingScreen";
 import AdminLayout from "./components/AdminLayout";
 
-const App = () => {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/dashboard" element={<AdminLayout />}>
-        {routes.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Route>
-      <Route>
-        {authRoutes.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Route>
-    </Routes>
+    <Suspense fallback={<LoadingScreen />}>
+      <Routes>
+        <Route path="/dashboard" element={<AdminLayout />}>
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
+        <Route>
+          {authRoutes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
+      </Routes>
+    </Suspense>
   );
-};
-
-export default App;
+}
