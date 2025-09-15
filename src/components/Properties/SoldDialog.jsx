b99ce248564841130190e40bd0896fe829c8ecbf
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-15 20:23:42 +0530

    feat: enhance SoldDialog to display rented status with appropriate banner

--- src/components/Properties/SoldDialog.jsx
+++ src/components/Properties/SoldDialog.jsx
@@ -6,20 +6,21 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import soldBanner from "../assets/sold-banner.png";
+import rentedBanner from "../assets/rented-banner.png";
 
-export const SoldDialog = ({ open, onOpenChange }) => {
+export const SoldDialog = ({ open, onOpenChange, isRented }) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="sm:max-w-sm text-center">
         <DialogHeader>
           <div className="flex flex-col items-center gap-4">
             <img
-              src={soldBanner}
+              src={isRented ? rentedBanner : soldBanner}
               alt="Sold"
               className="w-48 h-48 object-contain"
             />
             <DialogTitle className="text-xl font-bold text-green-800">
-              This Property is already Sold..!
+              This Property is already {isRented ? "Rented" : "Sold"}..!
             </DialogTitle>
           </div>
         </DialogHeader>
