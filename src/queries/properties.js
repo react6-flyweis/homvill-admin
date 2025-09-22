dd96ecc9d3acf220563da7f6d1736d5e53165b26
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-22 22:06:59 +0530

    feat: add useGetPropertyById hook for fetching property details by ID

--- src/queries/properties.js
+++ src/queries/properties.js
@@ -2,9 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import api from "@/lib/api";
 
 const GET_ALL_PROPERTIES = () => `/api/properties/getall`;
+const GET_PROPERTY_BY_ID = (id) => `/api/properties/getbyid/${id}`;
 
 async function fetchAllProperties() {
   const { data } = await api.get(GET_ALL_PROPERTIES());
+  return data;
+}
+
+async function fetchPropertyById(id) {
+  const { data } = await api.get(GET_PROPERTY_BY_ID(id));
   return data;
 }
 
@@ -21,6 +27,19 @@ export function useGetAllProperties(options = {}) {
           ? res.data.length
           : 0,
     }),
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
+  });
+}
+
+export function useGetPropertyById(id, options = {}) {
+  return useQuery({
+    queryKey: ["property", id],
+    queryFn: () => fetchPropertyById(id),
+    enabled: !!id,
+    select: (res) => res?.data || null,
     onError: (err) => {
       if (options.onError) options.onError(err);
     },
