25ddc247189d2995592b56c78171f653ba8e0eb3
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-22 22:07:14 +0530

    feat: implement PropertyDetails component to fetch and display property details

--- src/pages/Properties/PropertyDetails.jsx
+++ src/pages/Properties/PropertyDetails.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { PageLayout } from "@/components/layouts/PageLayout";
 import { PropertyDetailsContent } from "@/components/Properties/PropertyDetailsContent";
+import { useGetPropertyById } from "@/queries/properties";
 
 export default function PropertyDetails() {
+  const { id } = useParams();
+
+  const { data: property, isLoading, isError } = useGetPropertyById(id);
+
   return (
     <PageLayout
       title="Property Details"
       description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     >
-      <PropertyDetailsContent />
+      <PropertyDetailsContent
+        property={property}
+        loading={isLoading}
+        error={isError}
+      />
     </PageLayout>
   );
 }
