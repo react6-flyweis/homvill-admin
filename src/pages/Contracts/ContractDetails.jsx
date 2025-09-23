b2b54e5752dd2ef7d75215e4bdf13a8b068c1b88
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-23 21:00:01 +0530

    feat: update ContractDetails component to fetch and display contractor details

--- src/pages/Contracts/ContractDetails.jsx
+++ src/pages/Contracts/ContractDetails.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { PageLayout } from "@/components/layouts/PageLayout";
 import { PropertyDetailsContent } from "@/components/Properties/PropertyDetailsContent";
+import { useGetContractorPersonById } from "@/queries/contracts";
 
 export default function ContractDetails() {
-  //   const { id } = useParams();
+  const { id } = useParams();
+
+  const { data, isLoading, isFetching, error } = useGetContractorPersonById(id);
 
   return (
     <PageLayout
       title="contract Details"
       description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     >
-      {/* Render PropertyDetailsContent and show the enquiry card by default */}
-      <PropertyDetailsContent contract={{ category: "plumbing" }} />
+      <PropertyDetailsContent
+        property={data?.property_id}
+        contract={{ category: "plumbing" }}
+        loading={isLoading || isFetching}
+        error={error}
+      />
     </PageLayout>
   );
 }
