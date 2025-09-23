f2ac1e586a3cc0c32ffc0323bb7b04720b944a35
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-23 21:06:34 +0530

    feat: add useGetSubscriptionById hook for fetching subscription details by ID

--- src/queries/subscriptions.js
+++ src/queries/subscriptions.js
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import api from "@/lib/api";
 
 const GET_ALL_SUBSCRIPTIONS = () => `/api/subscriptions/getall`;
+const GET_SUBSCRIPTION_BY_ID = (id) => `/api/subscriptions/getbyid/${id}`;
 
 async function fetchAllSubscriptions() {
   const { data } = await api.get(GET_ALL_SUBSCRIPTIONS());
@@ -13,6 +14,24 @@ export function useGetAllSubscriptions(options = {}) {
     queryKey: ["subscriptions"],
     queryFn: () => fetchAllSubscriptions(),
     select: (res) => ({ items: res?.data || [], count: res?.count || 0 }),
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
+  });
+}
+
+async function fetchSubscriptionById(id) {
+  const { data } = await api.get(GET_SUBSCRIPTION_BY_ID(id));
+  return data;
+}
+
+export function useGetSubscriptionById(id, options = {}) {
+  return useQuery({
+    queryKey: ["subscription", id],
+    queryFn: () => fetchSubscriptionById(id),
+    enabled: Boolean(id),
+    select: (res) => res?.data || res || {},
     onError: (err) => {
       if (options.onError) options.onError(err);
     },
