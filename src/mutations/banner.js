06a53937a9984bc2fbda9d9b6a83dda4599ece79
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-23 20:48:58 +0530

    feat: add useCreateBanner hook for creating banners via API

---
+++ src/mutations/banner.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import api from "@/lib/api";
+
+async function createBannerApi(payload) {
+  const { data } = await api.post("/api/banners/create", payload);
+  return data;
+}
+
+export function useCreateBanner(options = {}) {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: createBannerApi,
+    onSuccess: (res) => {
+      // invalidate banners list so it refetches after create
+      qc.invalidateQueries({ queryKey: ["banners"] });
+      if (options.onSuccess) options.onSuccess(res);
+    },
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
+  });
+}
