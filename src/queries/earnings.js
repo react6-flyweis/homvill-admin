7b89fc7a2513527d8445123d4f95caaf4464232a
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-25 01:05:45 +0530

    feat: add useGetEarningById hook for fetching earnings by ID

--- src/queries/earnings.js
+++ src/queries/earnings.js
@@ -8,11 +8,32 @@ async function fetchAllEarnings() {
   return data;
 }
 
+const GET_EARNING_BY_ID = (id) => `/api/earning/getbyid/${id}`;
+
+async function fetchEarningById(id) {
+  if (!id) throw new Error("Missing id");
+  const { data } = await api.get(GET_EARNING_BY_ID(id));
+  return data;
+}
+
 export function useGetAllEarnings(options = {}) {
   return useQuery({
     queryKey: ["earnings"],
     queryFn: () => fetchAllEarnings(),
     select: (res) => ({ items: res?.data || [], count: res?.count || 0 }),
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
+  });
+}
+
+export function useGetEarningById(id, options = {}) {
+  return useQuery({
+    queryKey: ["earning", id],
+    queryFn: () => fetchEarningById(id),
+    enabled: !!id,
+    select: (res) => res?.data || res || null,
     onError: (err) => {
       if (options.onError) options.onError(err);
     },
