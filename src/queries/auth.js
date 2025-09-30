6be6e12552c482dbd71c29326a253a082c472140
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-30 18:29:19 +0530

    feat: add verifyForgotPasswordOtp function and hook for OTP verification

--- src/queries/auth.js
+++ src/queries/auth.js
@@ -4,6 +4,7 @@ import { useAuthStore } from "@/store/authStore";
 
 const ADMIN_LOGIN = "/api/user/admin-login";
 const SEND_FORGOT_PASSWORD_OTP = "/api/user/forget-password/send-otp";
+const VERIFY_FORGOT_PASSWORD_OTP = "/api/user/forget-password/verify-otp";
 
 async function adminLogin(payload) {
   const { data } = await api.post(ADMIN_LOGIN, payload);
@@ -29,5 +30,16 @@ async function sendForgotPasswordOtp(payload) {
 export function useSendForgetPasswordOtp() {
   return useMutation({
     mutationFn: sendForgotPasswordOtp,
+  });
+}
+
+async function verifyForgotPasswordOtp(payload) {
+  const { data } = await api.post(VERIFY_FORGOT_PASSWORD_OTP, payload);
+  return data;
+}
+
+export function useVerifyForgetPasswordOtp() {
+  return useMutation({
+    mutationFn: verifyForgotPasswordOtp,
   });
 }
