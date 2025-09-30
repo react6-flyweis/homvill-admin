3a2f17a1a233e10d5c79b68cf6dcedc5cd56f600
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-30 22:33:40 +0530

    feat: add useUpdateSubadminPermission hook for updating subadmin permissions

--- src/mutations/subadminPermission.js
+++ src/mutations/subadminPermission.js
@@ -22,3 +22,29 @@ export function useCreateSubadminPermission(options = {}) {
     ...options,
   });
 }
+
+async function updatePermissionApi(payload) {
+  const { data } = await api.put("/api/subadmin-permission/update", payload);
+  return data;
+}
+
+export function useUpdateSubadminPermission(options = {}) {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: updatePermissionApi,
+    onSuccess: (res) => {
+      // refresh permissions for the updated user if available
+      if (res && res.data && res.data.user_id) {
+        qc.invalidateQueries({
+          queryKey: ["subadmin-permission", res.data.user_id],
+        });
+      }
+      if (options.onSuccess) options.onSuccess(res);
+    },
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
+  });
+}
