11f87f595513bf080b71b7e299a16dcacdd17109
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-09-30 18:29:56 +0530

    feat: add resetForgotPassword function and hook for password reset functionality

--- src/queries/auth.js
+++ src/queries/auth.js
@@ -5,6 +5,7 @@ import { useAuthStore } from "@/store/authStore";
 const ADMIN_LOGIN = "/api/user/admin-login";
 const SEND_FORGOT_PASSWORD_OTP = "/api/user/forget-password/send-otp";
 const VERIFY_FORGOT_PASSWORD_OTP = "/api/user/forget-password/verify-otp";
+const RESET_FORGOT_PASSWORD = "/api/user/forget-password/reset-password";
 
 async function adminLogin(payload) {
   const { data } = await api.post(ADMIN_LOGIN, payload);
@@ -41,5 +42,23 @@ async function verifyForgotPasswordOtp(payload) {
 export function useVerifyForgetPasswordOtp() {
   return useMutation({
     mutationFn: verifyForgotPasswordOtp,
+  });
+}
+
+async function resetForgotPassword(payload) {
+  const { data } = await api.post(RESET_FORGOT_PASSWORD, payload);
+  return data;
+}
+
+export function useResetForgetPassword(options = {}) {
+  return useMutation({
+    mutationFn: resetForgotPassword,
+    onSuccess: (res) => {
+      if (options.onSuccess) options.onSuccess(res);
+    },
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
   });
 }
