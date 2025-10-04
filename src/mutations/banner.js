181703193eb4db44a27f2a60564e9993e770eb98
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-04 23:00:49 +0530

    feat: implement useUpdateBanner hook for updating banners

--- src/mutations/banner.js
+++ src/mutations/banner.js
@@ -22,3 +22,24 @@ export function useCreateBanner(options = {}) {
     ...options,
   });
 }
+
+async function updateBannerApi(payload) {
+  const { data } = await api.put("/api/banners/update", payload);
+  return data;
+}
+
+export function useUpdateBanner(options = {}) {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateBannerApi,
+    onSuccess: (res) => {
+      qc.invalidateQueries({ queryKey: ["banners"] });
+      if (options.onSuccess) options.onSuccess(res);
+    },
+    onError: (err) => {
+      if (options.onError) options.onError(err);
+    },
+    ...options,
+  });
+}
